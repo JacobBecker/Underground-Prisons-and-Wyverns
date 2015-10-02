6a5624afa584681f7107bea461849a6cd11ae2e8
Carly Hoffman <hoffman.carly@charterschool.org>
2015-10-02 14:06:26 -0400

    Documentation

--- Underground-Prisons-and-Wyverns/src/Project/Enemy.java
+++ Underground-Prisons-and-Wyverns/src/Project/Enemy.java
@@ -1,10 +1,12 @@
-public class Enemy {
-int health;
-int strength;
-String name;
-public void setEnemy(String n, int h, int s){
-	name = n;
-	health = h;
-	strength = s;
-}
+public class Enemy 
+{
+	int health;
+	int strength;
+	String name;
+	public void setEnemy(String n, int h, int s)
+	{
+		name = n;
+		health = h;
+		strength = s;
+	}
 }
