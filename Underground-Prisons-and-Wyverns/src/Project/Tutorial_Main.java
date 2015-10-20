d872e35d91287478057870d23dd97bf8d8893e24
Jacob Becker <becker.jacob@charterschool.org>
2015-10-20 14:22:28 -0400

    More attempts at Skins

--- Underground-Prisons-and-Wyverns/src/Project/Tutorial_Main.java
+++ Underground-Prisons-and-Wyverns/src/Project/Tutorial_Main.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 import Project.Tutorial_Game;
 
@@ -22,16 +24,14 @@ public class Tutorial_Main {
 		//Gdx.input.getTextInput(listener, "Dialog Title", "Test Text", null);
 		//Text Input
 		
-		/*
-		Skin skin = new Skin();
 		
-		new Dialog("Some Dialog", skin, "dialog") {
-		    protected void result (Object object) {
-		        System.out.println("Chosen: " + object);
-		    }
-		}.text("Are you enjoying this demo?").button("Yes", true).button("No", false).key(Keys.ENTER, true)
-		    .key(Keys.ESCAPE, false).show(stage);
-		    */
+		Skin skin = new Skin(Gdx.files.internal("/git/Underground-Prisons-and-Wyverns/Underground-Prisons-and-Wyverns/assets/uiskin.json"));
+
+		Stage stage = new Stage();
+		TextButton button = new TextButton("Click Me!", skin);
+		stage.addActor(button);
+
+		    
 		//Struggling with skins
 
 		
