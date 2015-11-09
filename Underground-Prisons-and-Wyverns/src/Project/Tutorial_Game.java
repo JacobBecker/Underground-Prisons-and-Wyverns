ca8baca32ee5ccad4e1f1cae0a15d520f7622614
Carly <hoffman.carly@charterschool.org>
2015-11-09 10:15:06 -0500

    Carly trying to label what stuff is doing again, feel free to correct or add more.

--- Underground-Prisons-and-Wyverns/src/Project/Tutorial_Game.java
+++ Underground-Prisons-and-Wyverns/src/Project/Tutorial_Game.java
@@ -78,21 +78,25 @@ public class Tutorial_Game implements ApplicationListener {
 			sr.rect(ROOM_WIDTH*place.x+cam_pos_x,ROOM_HEIGHT*place.y+cam_pos_y,ROOM_WIDTH,ROOM_HEIGHT);
 		}
 		sr.setColor(1,0,0,1);
-		sr.circle(25+ROOM_WIDTH*x_pos+cam_pos_x, 25+ROOM_WIDTH*y_pos+cam_pos_y, 5);
+		sr.circle(25+ROOM_WIDTH*x_pos+cam_pos_x, 25+ROOM_WIDTH*y_pos+cam_pos_y, 5);//pretty sure this is the little character circle
 		if(direction==0)
 		{
+			//the little triangle showing you which way you're going/facing
 			sr.triangle(25+ROOM_WIDTH*x_pos+cam_pos_x+ROOM_WIDTH/5, 25+ROOM_WIDTH*y_pos+cam_pos_y-ROOM_HEIGHT/5, 25+ROOM_WIDTH*x_pos+cam_pos_x+ROOM_WIDTH/5, 25+ROOM_WIDTH*y_pos+cam_pos_y+ROOM_HEIGHT/5, 25+ROOM_WIDTH*x_pos+cam_pos_x+2*ROOM_WIDTH/5, 25+ROOM_WIDTH*y_pos+cam_pos_y);
 		}
 		else if(direction==1)
 		{
+			//the little triangle showing you which way you're going/facing
 			sr.triangle(25+ROOM_WIDTH*x_pos+cam_pos_x-ROOM_WIDTH/5, 25+ROOM_WIDTH*y_pos+cam_pos_y+ROOM_HEIGHT/5, 25+ROOM_WIDTH*x_pos+cam_pos_x+ROOM_WIDTH/5, 25+ROOM_WIDTH*y_pos+cam_pos_y+ROOM_HEIGHT/5, 25+ROOM_WIDTH*x_pos+cam_pos_x, 25+ROOM_WIDTH*y_pos+cam_pos_y+2*ROOM_HEIGHT/5);
 		}
 		else if(direction==2)
 		{
+			//the little triangle showing you which way you're going/facing
 			sr.triangle(25+ROOM_WIDTH*x_pos+cam_pos_x-ROOM_WIDTH/5, 25+ROOM_WIDTH*y_pos+cam_pos_y-ROOM_HEIGHT/5, 25+ROOM_WIDTH*x_pos+cam_pos_x-ROOM_WIDTH/5, 25+ROOM_WIDTH*y_pos+cam_pos_y+ROOM_HEIGHT/5, 25+ROOM_WIDTH*x_pos+cam_pos_x-2*ROOM_WIDTH/5, 25+ROOM_WIDTH*y_pos+cam_pos_y);
 		}
 		else
 		{
+			//the little triangle showing you which way you're going/facing
 			sr.triangle(25+ROOM_WIDTH*x_pos+cam_pos_x-ROOM_WIDTH/5, 25+ROOM_WIDTH*y_pos+cam_pos_y-ROOM_HEIGHT/5, 25+ROOM_WIDTH*x_pos+cam_pos_x+ROOM_WIDTH/5, 25+ROOM_WIDTH*y_pos+cam_pos_y-ROOM_HEIGHT/5, 25+ROOM_WIDTH*x_pos+cam_pos_x, 25+ROOM_WIDTH*y_pos+cam_pos_y-2*ROOM_HEIGHT/5);
 		}
 		sr.end();
@@ -104,9 +108,9 @@ public class Tutorial_Game implements ApplicationListener {
 			sr.rect(ROOM_WIDTH*place.x+cam_pos_x,ROOM_HEIGHT*place.y+cam_pos_y,ROOM_WIDTH,ROOM_HEIGHT);
 		}
 		sr.end();
-		if(!cameraMode)
+		if(!cameraMode)//if you haven't pressed c (camera is based on character)
 		{
-			if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){//if you press the right key, you move to the right once
 				x_pos++;
 				direction=0;
 				boolean valid=false;
@@ -118,11 +122,11 @@ public class Tutorial_Game implements ApplicationListener {
 						place.visited = true;
 					}
 				}
-				if(!valid)
+				if(!valid)//if this movement is not possible, you do not move at all
 				{
 					x_pos--;
 				}
-				else
+				else//if the movement is valid, the camera will move based on location
 				{
 					if(x_pos+reset_cam_x/ROOM_WIDTH>=WIDTH/ROOM_WIDTH)//!
 					{
@@ -131,7 +135,7 @@ public class Tutorial_Game implements ApplicationListener {
 					}
 				}
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){//if you press the left arrow key, you will move to the left once
 				x_pos--;
 				direction=2;
 				boolean valid=false;
@@ -143,11 +147,11 @@ public class Tutorial_Game implements ApplicationListener {
 						place.visited = true;
 					}
 				}
-				if(!valid)
+				if(!valid)//if this movement is impossible, you don't move at all
 				{
 					x_pos++;
 				}
-				else
+				else//if it is valid, move or leave the camera based on location
 				{
 					if(-1*reset_cam_x/ROOM_WIDTH>x_pos)
 					{
@@ -156,7 +160,7 @@ public class Tutorial_Game implements ApplicationListener {
 					}
 				}
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.UP)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.UP)){//if the up arrow is pressed, move up once
 				y_pos++;
 				direction=1;
 				boolean valid=false;
@@ -168,11 +172,11 @@ public class Tutorial_Game implements ApplicationListener {
 						place.visited = true;
 					}
 				}
-				if(!valid)
+				if(!valid)//if this movement is not valid, don't move
 				{
 					y_pos--;
 				}
-				else
+				else//if the movement is valid move the camera 
 				{
 					if(y_pos+reset_cam_y/ROOM_HEIGHT>=HEIGHT/ROOM_HEIGHT)
 					{
@@ -181,7 +185,7 @@ public class Tutorial_Game implements ApplicationListener {
 					}
 				}
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)){//if the down arrow is pressed, then move down one
 				y_pos--;
 				direction=3;
 				boolean valid=false;
@@ -193,11 +197,11 @@ public class Tutorial_Game implements ApplicationListener {
 						place.visited = true;
 					}
 				}
-				if(!valid)
+				if(!valid)//if this movement is not valid, then do not move
 				{
 					y_pos++;
 				}
-				else
+				else//if it is valid, modify the camera
 				{
 					if(-1*reset_cam_y/ROOM_HEIGHT>y_pos)
 					{
@@ -206,17 +210,17 @@ public class Tutorial_Game implements ApplicationListener {
 					}
 				}
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.S)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.S)){//if you press s, you can move the directional arrow clockwise
 				direction--;
 				if(direction<0)
 				{
 					direction = 3;
 				}
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.A)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.A)){//if you press a, you can move the directional arrow counter-clockwise
 				direction = (direction+1)%4;
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.C)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.C)){//if you press c, you can move the camera
 				cameraMode=true;
 				System.out.println("Camera Mode on");
 				//reset_cam_x = cam_pos_x;
@@ -225,7 +229,7 @@ public class Tutorial_Game implements ApplicationListener {
 		}
 		else
 		{
-			if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){//if you press the right arrow
 				//System.out.println("X: "+cam_pos_x+"/n"+"Y: "+cam_pos_y);
 				cam_pos_x -= ROOM_WIDTH;
 				if(cam_pos_x<=WIDTH-WORLD_WIDTH)
@@ -233,7 +237,7 @@ public class Tutorial_Game implements ApplicationListener {
 					cam_pos_x = WIDTH-WORLD_WIDTH;
 				}
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){//if you press the left arrow 
 				//System.out.println("X: "+cam_pos_x+"/n"+"Y: "+cam_pos_y);
 				cam_pos_x += ROOM_WIDTH;
 				if(cam_pos_x>=0)
@@ -241,7 +245,7 @@ public class Tutorial_Game implements ApplicationListener {
 					cam_pos_x = 0;
 				}
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.UP)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.UP)){//if you press the up arrow
 				//System.out.println("X: "+cam_pos_x+"/n"+"Y: "+cam_pos_y);
 				cam_pos_y -= ROOM_HEIGHT;
 				if(cam_pos_y<=HEIGHT-WORLD_HEIGHT)
@@ -249,7 +253,7 @@ public class Tutorial_Game implements ApplicationListener {
 					cam_pos_y = HEIGHT-WORLD_HEIGHT;
 				}
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)){//if you press the down arrow
 				//System.out.println("X: "+cam_pos_x+"/n"+"Y: "+cam_pos_y);
 				cam_pos_y += ROOM_HEIGHT;
 				if(cam_pos_y>=0)
@@ -257,7 +261,7 @@ public class Tutorial_Game implements ApplicationListener {
 					cam_pos_y = 0;
 				}
 			}
-			if(Gdx.input.isKeyJustPressed(Input.Keys.C)){
+			if(Gdx.input.isKeyJustPressed(Input.Keys.C)){//if you press C a second time, turn off mobile camera
 				cameraMode=false;
 				System.out.println("Camera Mode off");
 				cam_pos_x = reset_cam_x;
